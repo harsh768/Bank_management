70a26748da3dfa21339be49355bc97d62df434a4
Harsh <guptaharsh768@gmail.com>
2020-09-21 12:07:08 +0530

    Added localhost and some basic routes and controllers

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+//Baisc express needs 
 const express=require('express');
-const cookieParser = require('cookie-parser');
 const app=express();
 const port = 8000;
+
+//Middlewares
+app.use(express.urlencoded());
+
+//accessing static files
+app.use(express.static('assets'));
+
+//Views 
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+//use express router
+app.use('/',require('./routes'));
+
+//Running the server up!
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+})
